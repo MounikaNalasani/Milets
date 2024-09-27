638d5072bea743f5d13d92294f761bde25c76333
MounikaNalasani <mounikanallasani21@gmail.com>
2024-09-27 21:10:44 +0530

    milets

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+
+
+  $(document).ready(function() {
+    
+    $('#myCarousel').carousel({
+      interval: 3000, 
+      wrap: true 
+    });
+    $('#cardCarousel').carousel({
+      interval: 3000,
+      
+    });
+  });
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
